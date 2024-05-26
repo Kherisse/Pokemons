592c95d826dbe650cee394557cf76d278ce49331
Kherisse <cherrie141922@gmail.com>
2024-05-27 01:31:41 +0800

    [updated pet details]

--- src/components/PetDetails.jsx
+++ src/components/PetDetails.jsx
@@ -20,12 +20,18 @@ export default function PetDetails() {
 
   return (
     // <div className="col-span-1  grid w-full sm:grid-cols-1 lg:grid-cols-1 md:grid-cols-1 gap-2 p-10 bg-sky-500/100 sm:mx-auto rounded-md ">
-    <div className="grid col-span-3 w-full sm:grid-cols-1 gap-2 p-10 sm:mx-auto rounded-md bg-indigo-800 bg-opacity-50">
-      <div>
-        <h1 className="underline">POKEMON DETAILSsss</h1>
-        <img src={petData?.sprites?.front_default} alt="" />
-        <h1>{petData?.name}</h1>
+    <div className="grid col-span-3 w-full sm:grid-cols-1 gap-2 p-10 sm:mx-auto rounded-md bg-indigo-800 bg-opacity-50 ">
+      <div className="text-center">
+        <h1 className="text-red text-4xl font-bold">POKEMON DETAILS</h1>
+        <img
+          className="h-72 mx-auto mt-5 border-violet-700 rounded-lg border-2"
+          src={petData?.sprites?.front_default}
+          alt=""
+        />
+        <h1 className="font-bold mt-5 uppercase">{petData?.name}</h1>
         <h1>{petData?.id}</h1>
+        <div className="divider"></div>
+        <h1>Weight: {petData?.weight}</h1>
       </div>
     </div>
   );
