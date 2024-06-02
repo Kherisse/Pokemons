dc7c01f0c1ac4d5e09e04e0e07555157bef01e30
Kherisse <cherrie141922@gmail.com>
2024-06-02 22:41:26 +0800

    [updated pet div]

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -2,12 +2,9 @@ import pokes from "../assets/pokemon1.png";
 
 export default function Homehero() {
   return (
-    <div className="hero w-full mt-10 bg-base-200">
-      <div className="hero-content flex-col-2 lg:flex-row sm:w-40 md:w-full">
-        <img
-          src={pokes}
-          className="sm:max-w-sm rounded-lg shadow-2xl sm:w-full "
-        />
+    <div className="hero w-full mt-10 bg-base-200 mx-auto">
+      <div className="hero-content  flex flex-wrap">
+        <img src={pokes} className="sm:max-w-sm rounded-lg shadow-2xl " />
         <div className="mx-auto text-center">
           <h1 className="text-5xl font-bold sm:text-sm">Box Office News!</h1>
           <p className="py-6 text-sm">
